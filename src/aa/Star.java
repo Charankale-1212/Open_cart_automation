2b14b3792f67ff1999b15ff34c7f34aff926edd9
charan <charanbkale9890@gmail.com>
2023-08-19 19:17:04 +0530

    First commit

---
+++ src/aa/Star.java
@@ -0,0 +1,17 @@
+package aa;
+
+public class Star 
+{
+	public static void main(String[] args) 
+	{
+		
+		for(int i=4;i<=4;i++)
+	      {
+		  for(int j=1;j>=i;j++)
+		    
+		  System.out.println();
+	      }
+		
+	}
+
+}
